f97e5be44621d9c8aa6ca4e02eb12c61dd6f6c3f
acuervoa <kuerbo@gmail.com>
2016-09-23 22:12:59 +0200

    Add load Audios and keyboard

--- piano.js
+++ piano.js
@@ -12,6 +12,74 @@ function PianoApp()
 	this.start = function() {
 		$("#app>header").append(version);
 		setStatus("ready");
+		loadAudio();
+		initKeyboard();
+	}
+
+	function loadAudio()
+	{
+		var count = 0,
+		loaded = 0,
+		error = false;
+
+		$("#keyboard .piano-key").each(function()
+			{
+				count++;
+				var noteName = escape($(this).data("note"));
+				audioManager.getAudio(noteName, 
+					function()
+					{
+						if (error) return;
+						if (++loaded == count) setStatus("Ready.");
+						else setStatus("Loading " + Math.floor(100 * loaded / count) + "%");
+					},
+					function(audio)
+					{
+						error = true;
+ 						setStatus("Error loading: " + audio.src);
+					}
+				);
+			});
+	}
+
+	function initKeyboard()
+	{
+		var $keys = $("#keyboard .piano-key");
+		if ($.isTouchSupported)
+		{
+			$keys.touchstart(function(e) {
+				e.stopPropagation();
+				e.preventDefault();
+				keyDown($(this));
+			})
+			.touchend(function() { keyUp($(this)); })
+		}
+		else
+		{
+			$keys.mousedown(function() {
+				keyDown($(this));
+				return false;
+			})
+			.mouseup(function() { keyUp($(this)); })
+			.mouseleave(function() { keyUp($(this)); });
+		}
+	}
+
+	function keyDown($key)
+	{
+		if(!$key.hasClass("down"))
+		{
+			$key.addClass("down");
+			var noteName = $key.data("note");
+			var audio = audioManager.getAudio(escape(noteName));
+			audio.currentTime = 0;
+			//audio.play();
+		}
+	}
+
+	function keyUp($key) 
+	{
+		$key.removeClass("down");
 	}
 }
 
